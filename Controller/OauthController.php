4fb0388c1315d177215a8a67d2d10c501cf8f17a
Miro Cillik <miro@keboola.com>
2015-01-07 13:12:18 +0000

    fix: error when user tries to authorize himself into his config which is external

--- Controller/OauthController.php
+++ Controller/OauthController.php
@@ -161,7 +161,12 @@ class OauthController extends BaseController
 			;
 
 			if ($account->isExternal()) {
-				$account->setOwner($tokenData['creatorToken']['description']);
+				if ($userEmail == $tokenData['description'] || !isset($tokenData['creatorToken'])) {
+					// user generated an external link for himself or is reauthorizing himself into his config which was external before
+					$account->setExternal(false);
+				} else {
+					$account->setOwner($tokenData['creatorToken']['description']);
+				}
 			}
 
 			$account->save();
